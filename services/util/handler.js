b21539bcabdd28c402e35f5bf068e5629bb49a3a
Maria Castro <maria@Marias-MacBook-Pro-2.local>
2022-08-24 12:51:37 +0100

    Adding the API

---
+++ services/util/handler.js
@@ -0,0 +1,23 @@
+
+// wrapper for our Lambda functions. It takes our Lambda function as the argument.
+export default function handler(lambda) {
+    return async function (event, context) {
+      let body, statusCode;
+  
+      try {
+        // Run the Lambda
+        body = await lambda(event, context);
+        statusCode = 200;
+      } catch (e) {
+        console.error(e);
+        body = { error: e.message };
+        statusCode = 500;
+      }
+  
+      // Return HTTP response
+      return {
+        statusCode,
+        body: JSON.stringify(body),
+      };
+    };
+  }
---
+++ stacks/ApiStack.js
@@ -0,0 +1,43 @@
+import { Api, use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack, app }) {
+    //  references the table resource from the StorageStack
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        // giving our API permission to access our DynamoDB table 
+        permissions: [table],
+        environment: {
+            //passing in the name of our DynamoDB table
+          TABLE_NAME: table.tableName,
+        },
+      },
+    },
+    routes: {
+        // route to create a note
+      "POST /notes": "functions/create.main",
+        // route to get specific note
+      "GET /notes/{id}": "functions/get.main",
+        // route to get all notes
+      "GET /notes": "functions/list.main",
+        // route  for the get note API (get.js)
+      "PUT /notes/{id}": "functions/update.main",
+        // route to delete a note
+      "DELETE /notes/{id}": "functions/delete.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}
